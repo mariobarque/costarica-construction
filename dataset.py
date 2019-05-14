2a2d60c23d6e40cfe9b27d63c3cee5b6d0f06ad8
Barrantes Quesada <mario.alberto.barrantes.quesada@intel.com>
2019-05-14 14:11:28 -0600

    Adding dataset.py

---
+++ dataset.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+all_columns = ['anoper', 'num_obras', 'arecon', 'valobr', 'claper', 'claobr', 'numpis', 'numviv', 'numapo', 'numdor',
+               'matpis', 'matpar', 'mattec', 'usoobr', 'financ', 'cod_provincia', 'provincia', 'id_canton', 'canton',
+               'id_region']
+
+numeric_columns = ['num_obras', 'arecon', 'valobr', 'numpis', 'numviv', 'numapo', 'numdor']
+categorical_columns = ['anoper', 'claper', 'claobr', 'matpis', 'matpar', 'mattec', 'usoobr', 'financ',
+                       'cod_provincia', 'id_canton', 'id_region']
+
+
+def __init__(self):
+    self.data = pd.read_csv('data/construction-data-processed.csv')
+
+
+def get_data_set(self):
+    columns = numeric_columns + categorical_columns
+    df = self.data[columns]
+    return df
+
+
+def get_numerical_data_set(self):
+    df = self.data[numeric_columns]
+    return df
+
+
+def get_categorical_data_set(self):
+    df = self.data[categorical_columns]
+    return df
+
--- program.py
+++ program.py
@@ -1,21 +1,4 @@
-import pandas as pd
-#import seaborn as sns
-#import matplotlib.pyplot as plt
-#from matplotlib import style
-#style.use('ggplot')
-
-
-df = pd.read_csv("data/construction-data-processed.csv")
-
-
-#numeric_columns = ['valobr', '']
-
-df['valobr'].plot.kde()
-
-
-
-#sns.pairplot(df)
-
-#df.corr()
-
 print('Hi')
+
+
+
