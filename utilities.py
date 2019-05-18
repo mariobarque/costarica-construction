4d865f39a8aab8ca2f4ceef18baa4dc0b7579bc7
Mario Alberto Barrantes Quesada <mario@MacBook-Pro.local>
2019-05-17 22:14:28 -0600

    remove title

--- utilities.py
+++ utilities.py
@@ -32,11 +32,10 @@ def get_region(data):
     return 1
 
 
-def plot_distributions(df, numeric_columns, title):
+def plot_distributions(df, numeric_columns):
     if len(numeric_columns) != 6:
         return
     fig, axs = plt.subplots(ncols=3, nrows=2, squeeze=False)
-    fig.canvas.set_window_title(title)
 
     sns.distplot(df[numeric_columns[0]], ax=axs[0, 0])
     sns.distplot(df[numeric_columns[1]], ax=axs[0, 1])
