2905ae4d47741f51e2b16a7c90262a8c1b4744b8
Mario Alberto Barrantes Quesada <mario@MacBook-Pro.local>
2019-05-25 07:48:18 -0600

    refactor a bit regression model, removed error, so we can show error during test only

--- logisticregression_model.py
+++ logisticregression_model.py
@@ -2,45 +2,40 @@ import torch
 import torch.nn as nn
 
 
-def evaluateSigmoid(W, X):
-  NetWeights = torch.mm(X, W);
-  sigmoid = nn.Sigmoid();
-  Activation = sigmoid(NetWeights)
-  return Activation;
+def evaluate_sigmoid(W, X):
+    NetWeights = torch.mm(X, W)
+    sigmoid = nn.Sigmoid()
+    Activation = sigmoid(NetWeights)
+    return Activation
 
-def getDeltaWeightsForClass(W, Samples, alpha, target):
-  ActivationSamples = evaluateSigmoid(W, Samples)
-  Error = (target - ActivationSamples)
-  DeltaWeights = Samples * Error
-  DeltaWeightsWithAlpha = alpha * DeltaWeights
-  error = torch.sum(torch.abs(Error))
-  return DeltaWeightsWithAlpha, error
-
-
-def trainLogisticRegression(samplesClass1, samplesClass2, numIter, alpha):
-  samplesClass1 = torch.from_numpy(samplesClass1).float()
-  samplesClass2 = torch.from_numpy(samplesClass2).float()
-
-  dimensionsData = samplesClass1.size()
-  W = torch.ones(dimensionsData[1],1)
-
-  errorPerEpoch = []
-  for i in range(0, numIter):
-    (DeltaWeights1, error1) = getDeltaWeightsForClass(W, samplesClass1, alpha, target = 0)
-    (DeltaWeights2, error2) = getDeltaWeightsForClass(W, samplesClass2, alpha, target = 1)
-    error = error1 + error2
-    errorPerEpoch.append(float(error.numpy()))
-    WdeltaTotal = torch.sum(DeltaWeights1, dim = 0) + torch.sum(DeltaWeights2, dim = 0)
-    W = W + WdeltaTotal.view(len(W), 1)
-
-  return W.numpy().T[0], errorPerEpoch
+def get_delta_weights_for_class(weights, samples, alpha, target):
+    activation_samples = evaluate_sigmoid(weights, samples)
+    error = (target - activation_samples)
+    delta_weights = samples * error
+    delta_weights_with_alpha = alpha * delta_weights
+    return delta_weights_with_alpha
 
 
-def train(df, variable):
+def train_logistic_regression(class_zeros, class_ones, numIter, alpha):
+    class_zeros = torch.from_numpy(class_zeros).float()
+    class_ones = torch.from_numpy(class_ones).float()
+
+    dimensionsData = class_zeros.size()
+    weights = torch.ones(dimensionsData[1],1)
+
+    for i in range(0, numIter):
+        delta_weights_zero = get_delta_weights_for_class(weights, class_zeros, alpha, target = 0)
+        delta_weights_one = get_delta_weights_for_class(weights, class_ones, alpha, target = 1)
+        delta_weight_total = torch.sum(delta_weights_zero, dim = 0) + torch.sum(delta_weights_one, dim = 0)
+        weights = weights + delta_weight_total.view(len(weights), 1)
+
+    return weights.numpy().T[0]
+
+
+def train(df, variable, iterations = 50, alpha = 0.1):
     classZero = df.loc[df[variable] == 0].drop(variable, 1)
     classOne = df.loc[df[variable] == 1].drop(variable, 1)
 
-    numIterations = 10;
-    alpha = 0.1;
+    weights = train_logistic_regression(classZero.values, classOne.values, iterations, alpha)
 
-    weights, errors = trainLogisticRegression(classZero.values, classOne.values, numIterations, alpha)
+    return weights
