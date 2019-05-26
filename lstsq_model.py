84476000eb06b0d1140b68dca53cbdb16d360154
Mario Alberto Barrantes Quesada <mario@MacBook-Pro.local>
2019-05-26 17:36:30 -0600

    Add code to remove warning

--- lstsq_model.py
+++ lstsq_model.py
@@ -13,6 +13,6 @@ def train(df, variable):
     b = df[variable].values
     current_train = df.drop(variable, 1)
     A = current_train.values
-    weights = np.linalg.lstsq(A, b)[0]
+    weights = np.linalg.lstsq(A, b, rcond=None)[0]
 
     return weights
